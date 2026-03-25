fe17a0a2a16983aeda8f1eda43075a478e345421
danggay1977 <danggay1977@gmail.com>
2026-03-25 12:35:07 +0700

    Minor adjustments to the architecture and changes to the player and enemy attack mechanics.

---
+++ Assets/Scripts/Components/Enemy/EnemyAnimation.cs
@@ -0,0 +1,36 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class EnemyAnimation : NetworkBehaviour
+{
+    [SerializeField] private Animator anim;
+    private Vector3 originalScale;
+
+    public override void Spawned()
+    {
+        if (anim == null) anim = GetComponentInChildren<Animator>();
+        originalScale = transform.localScale;
+    }
+    public void UpdateSpeed(float speed)
+    {
+        if (anim != null) anim.SetFloat("Speed", speed);
+    }
+
+    public void FaceTarget(float targetX)
+    {
+        float dirX = (targetX > transform.position.x) ? 1f : -1f;
+        transform.localScale = new Vector3(Mathf.Abs(originalScale.x) * dirX, originalScale.y, originalScale.z);
+    }
+
+    public void PlayAttack(int attackIndex)
+    {
+        if (anim != null)
+        {
+            anim.SetInteger("AttackIndex", attackIndex); 
+            anim.SetTrigger("Attack");
+        }
+    }
+
+    public void PlayHit() { if (anim != null) anim.SetTrigger("Hit"); }
+    public void PlayDeath() { if (anim != null) anim.SetBool("IsDead", true); }
+}
