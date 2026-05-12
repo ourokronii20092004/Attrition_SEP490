9d2c579beab6c5306bc5b7fc28b348cc69b91ab1
danggay1977 <danggay1977@gmail.com>
2026-05-12 11:12:56 +0700

    fix UI Menu

--- Assets/Scripts/Controllers/MenuButtonController.cs
+++ Assets/Scripts/Controllers/MenuButtonController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,8 +10,21 @@ public class MenuButtonController : MonoBehaviour {
 	[SerializeField] int maxIndex;
 	public AudioSource audioSource;
 
+	[Tooltip("Tự động đếm số MenuButton trong scene để set maxIndex")]
+	[SerializeField] bool autoDetectMaxIndex = true;
+
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
+
+		// Auto detect: tìm tất cả MenuButton trong scene và set maxIndex
+		if (autoDetectMaxIndex)
+		{
+			MenuButton[] buttons = FindObjectsOfType<MenuButton>();
+			if (buttons.Length > 0)
+			{
+				maxIndex = buttons.Length - 1;
+			}
+		}
 	}
 	
 	// Update is called once per frame
